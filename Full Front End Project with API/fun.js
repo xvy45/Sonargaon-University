4ee16fa7260305df31c4fa707a8a1e30147f68b7
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-07-11 17:47:03 +0600

    Update fun.js

--- Full Front End Project with API/fun.js
+++ Full Front End Project with API/fun.js
@@ -1,54 +1,64 @@
-// function handalSearch(){
-//     const SearchInputElement = document.getElementByIdd('search_input_filed');
-//     const searchInputValue = searchInputElament.value;
-
-//     loadPhone =(searchInputValue);
-// }
-
-// const loadPhone = async(searchText) => {
-//     const res = await fetch(
-//         `https://openapi.programming-hero.com/api/phone?search=${searchTex}`
-//     );
-
-//     console.log("Server Response: ", res);
-//     const serverData = await res.json();
-//     displayPhone(serverData.data);
-// };
-
-// const displayPhone = (data) => {
-//     // console.log("Inside displayPhone function: ", data);
-//     const cardContaner = document.getElementById("Card_Section");
-
-//     cardContaner.innerHTML = "";
-//     data.array.forEach( Phone => {
-//         const productCard = document.createElement("div");
-//         productCard.classList.add("card");
-//         console.log(productCard);
-
-//         productCard.innerHTML= `
-//         <div class="card_img">
-//                 <img src=${phone.image} alt="phone-img" />
-//             </div>
-//             <div>
-//                 <h3 class="cardTitle"> ${Phone.phone_name}</h3>
-//                 <p class="cardDecraption">There are many variations of passages of available, but the majority have suffered</p>
-//             </div>
-//             <div class="card_prize">
-//                 <span>$</span>
-//                 <span id="card_item_Prize">999</span>
-//             </div>
-//             <div class="card_btn">
-//                 <button class="btn">Show Details</button>
-//             </div>
-//         `;
-//         cardContaner.appendChild(productCard);
-//     });
-// };
-
-
+// https://openapi.programmig-hero.com/api/phones?search=samsung
 function handalSearch(){
-    const searchInputElament = document.getElementById('search_input_filed');
-    console.log(searchInputElament)
+    loadingAnimationTogol(true);
+    const searchInputElament = document.getElementById("search_input_filed");
+    const searchInputValue = searchInputElament.value;
+
+    loadPhone(searchInputValue);
 }
 
+function loadingAnimationTogol(isLoading) {
+    const loaderAnimation = document.getElementById("loderAnimation");
+    if(isLoading){
+        loaderAnimation.classList.remove("hidden");
+    }
+    else{
+        loaderAnimation.classList.add("hidden");
+    }
+}
 
+const loadPhone = async(searchText) => {
+    //console.log("Insite the load phone function: ",searchText);
+    const res = await fetch(
+        ` https://openapi.programming-hero.com/api/phones?search=${searchText} `);
+    
+    console.log("Server Response: ", res);
+
+    const Serverdata = await res.json();
+
+    displayPhone(Serverdata.data);
+}
+
+const displayPhone = (data) =>{
+    const cardContainer = document.getElementById("Card_Section");
+    console.log("server data", data)
+
+    cardContainer.innerHTML = "";
+    data.forEach(phone => {
+       // console.log(phone.phone_name);
+       const prodectCard = document.createElement("div");
+       prodectCard.classList.add("card");
+
+        prodectCard.innerHTML= 
+        `
+        <div class="card_img">
+                <img src=${phone.image} alt="phone img">
+            </div>
+            <div>
+                <h3 class="cardTitle">${phone.phone_name}</h3>
+                <p class="cardDecraption">There are many variations of passages of available, but the majority have suffered</p>
+            </div>
+            <div class="card_prize">
+                <span>$</span>
+                <span id="card_item_Prize">999</span>
+            </div>
+            <div class="card_btn">
+                <button class="btn">Show Details</button>
+            </div>
+        `;
+        
+        cardContainer.appendChild(prodectCard);
+       //console.log(prodectCard);
+    });
+    loadingAnimationTogol(false);
+};
